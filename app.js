59b585a57a120882f530d88a242ce70ba3ad9550
BaljeetSharma98 <baljeetsharma.abc@gmail.com>
2024-07-17 01:17:56 +0530

    app.js

--- app.js
+++ app.js
@@ -1,117 +1,166 @@
-const contractAddress = '0xb73A4e4a2FB06A8aAED22c7733ec0B3801BCb997';
-const contractABI = [{
-    "inputs": [
+const contractAddress = '0x70b00CADD9E6000e28D4E1F5760D7CA02ab2285C'.trim();
+const contractABI = [
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "result",
+      "outputs": [
         {
-            "internalType": "uint256",
-            "name": "_value1",
-            "type": "uint256"
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "a",
+          "type": "uint256"
         },
         {
-            "internalType": "uint256",
-            "name": "_value2",
-            "type": "uint256"
+          "internalType": "uint256",
+          "name": "b",
+          "type": "uint256"
         }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-},
-{
-    "inputs": [],
-    "name": "value1",
-    "outputs": [
+      ],
+      "name": "add",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
         {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-        }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-},
-{
-    "inputs": [],
-    "name": "value2",
-    "outputs": [
+          "internalType": "uint256",
+          "name": "a",
+          "type": "uint256"
+        },
         {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
+          "internalType": "uint256",
+          "name": "b",
+          "type": "uint256"
         }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-},
-{
-    "inputs": [
+      ],
+      "name": "subtract",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
         {
-            "internalType": "uint256",
-            "name": "_value1",
-            "type": "uint256"
-        }
-    ],
-    "name": "setValue1",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-},
-{
-    "inputs": [
+          "internalType": "uint256",
+          "name": "a",
+          "type": "uint256"
+        },
         {
-            "internalType": "uint256",
-            "name": "_value2",
-            "type": "uint256"
+          "internalType": "uint256",
+          "name": "b",
+          "type": "uint256"
         }
-    ],
-    "name": "setValue2",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-},
-{
-    "inputs": [],
-    "name": "getValue1",
-    "outputs": [
+      ],
+      "name": "multiply",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
         {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-        }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-},
-{
-    "inputs": [],
-    "name": "getValue2",
-    "outputs": [
+          "internalType": "uint256",
+          "name": "a",
+          "type": "uint256"
+        },
         {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
+          "internalType": "uint256",
+          "name": "b",
+          "type": "uint256"
         }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-}];
+      ],
+      "name": "divide",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getResult",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    }
+];
 
 async function load() {
     if (window.ethereum) {
         window.web3 = new Web3(window.ethereum);
-        await window.ethereum.enable();
+        try {
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+        } catch (error) {
+            console.error('User denied account access');
+        }
     } else {
         console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
     }
 
-    const accounts = await web3.eth.getAccounts();
-    const account = accounts[0];
+    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+    window.account = accounts[0];
+    console.log('Account:', window.account);
 
-    const myContract = new web3.eth.Contract(contractABI, contractAddress);
+    window.myContract = new web3.eth.Contract(contractABI, contractAddress);
+    console.log('Contract:', window.myContract);
 
-    const value1 = await myContract.methods.getValue1().call();
-    const value2 = await myContract.methods.getValue2().call();
+    updateResult();
+}
 
-    document.getElementById('value1').innerText = value1;
-    document.getElementById('value2').innerText = value2;
+async function performOperation(operation) {
+    const a = document.getElementById('operandA').value;
+    const b = document.getElementById('operandB').value;
+    console.log(`Performing ${operation} on:`, a, b);
+
+    if (!window.myContract) {
+        console.error('Contract not initialized');
+        return;
+    }
+
+    try {
+        await window.myContract.methods[operation](a, b).send({ from: window.account });
+        console.log(`${operation} performed successfully`);
+        updateResult();
+    } catch (error) {
+        console.error(`Error performing ${operation}:`, error);
+    }
+}
+
+async function updateResult() {
+    try {
+        const result = await window.myContract.methods.getResult().call();
+        console.log('Result:', result);
+        document.getElementById('result').innerText = result;
+    } catch (error) {
+        console.error('Error getting result:', error);
+    }
 }
 
 window.addEventListener('load', load);

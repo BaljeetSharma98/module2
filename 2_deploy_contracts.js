74bb12cf63a36bf26d3f01d1b2c105ba26e3f443
BaljeetSharma98 <baljeetsharma.abc@gmail.com>
2024-07-17 18:12:17 +0530

    Update 2_deploy_contracts.js

--- 2_deploy_contracts.js
+++ 2_deploy_contracts.js
@@ -1,9 +1,9 @@
-const Calculator = artifacts.require("./Calculator");
+const TaxCalculator = artifacts.require("./TaxCalculator");
 
 module.exports = async function (deployer) {
   try {
     console.log("Starting deployment...");
-    await deployer.deploy(Calculator);
+    await deployer.deploy(TaxCalculator);
     console.log("Contract deployed successfully!");
   } catch (error) {
     console.error("Deployment failed:", error);
